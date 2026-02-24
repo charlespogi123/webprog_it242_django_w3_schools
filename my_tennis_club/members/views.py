5bef474ec38374d5faabdbac043b9a1dc41db6f2
Charles B. Capagngan <cbcapagngan2@student.apc.edu.ph>
2026-02-24 23:40:16 +0800

    Update greeting message in members view

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("<h2>Hello! I am Charles!</h2>") 
+    return HttpResponse("<h2>Hello! I am Charles :D!</h2>") 
